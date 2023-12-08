62e0a7155e1e22677eb9238811226700adea2b97
koustavroy <koustav.roy@myelinfoundry.com>
2023-12-08 18:01:48 +0530

    inserting LinkList

--- LinkList.java
+++ LinkList.java
@@ -2,10 +2,21 @@ public class LinkList {
     public static void main (String[] args) throws java.lang.Exception
 	{
 	 SingleLinkList sll = new SingleLinkList();
-     sll.createSingleLinkedListnode(10);
-     System.out.println(sll.head.next);
-      System.out.println(sll.size);
-
-
+     LinkList ll = new LinkList();
+        sll.createSingleLinkedListnode(10);
+        ll.displayLinkList(sll.head);
+        sll.push(30, 2);
+         sll.push(40, 3);
+          sll.push(50, 4);
+           sll.push(60, 5);
+           sll.push(70, 2);
+        ll.displayLinkList(sll.head);
 	}
+    public void  displayLinkList(Node start){
+        while(start!=null){
+            System.out.print(start.value+"-");
+            start=start.next;
+        }
+        System.out.println();
+    }
 }
--- SingleLinkList.java
+++ SingleLinkList.java
@@ -16,4 +16,40 @@ public  class SingleLinkList
 		size++;
 		return head;
 	}
+
+	public void  push(int nodeVal, int loc){
+		Node node = new Node();
+		node.value=nodeVal;
+		if(head==null){
+			createSingleLinkedListnode(nodeVal);
+		}
+		else if(loc == 0){   // inserting in the beginning 
+			node.next=head;
+			head=node;
+		}
+		else if(loc>=size){   // inserting at the end 
+			node.next=null;
+			tail.next=node;
+			tail=node;
+		}
+		else {				// inserting in the middle 
+			int pos =0;
+			Node prev =head;
+			Node tmp = head;
+			while(tmp!=null){
+				pos++;
+				if(pos==loc){  // changing the value 
+					prev.next=node;
+					node.next=tmp.next;
+					break;
+				}else{
+					prev=tmp;
+					tmp=tmp.next;
+				}
+			}
+
+
+		}
+		size++;
+	}
 }
