d186c28c4e82e017f8856f84f56900a41f796326
koustavroy <koustav.roy@myelinfoundry.com>
2023-12-10 17:34:04 +0530

    Single Link List completed

--- LinkList.java
+++ LinkList.java
@@ -8,9 +8,22 @@ public class LinkList {
         sll.push(30, 2);
          sll.push(40, 3);
           sll.push(50, 4);
-           sll.push(60, 5);
+           sll.push(80, 4);
+            sll.push(90, 4);
+             sll.push(100, 4);
+            
            sll.push(70, 2);
         ll.displayLinkList(sll.head);
+
+
+        sll.pop(0);
+            ll.displayLinkList(sll.head);
+            sll.pop(4);
+            ll.displayLinkList(sll.head);
+              sll.pop(2);
+               ll.displayLinkList(sll.head);
+
+        
 	}
     public void  displayLinkList(Node start){
         while(start!=null){
--- SingleLinkList.java
+++ SingleLinkList.java
@@ -52,4 +52,51 @@ public  class SingleLinkList
 		}
 		size++;
 	}
+
+
+		public void  pop(int loc){
+
+
+		if(head==null){
+			System.out.println("No element to delete !!");
+		}
+		else if( size ==1){   // deleting if only 1 leemnt present 
+			head = null;
+			tail = null;
+		}
+		else if (loc==0){   //deleting from beginning 
+			head = head.next;
+		}
+		else if(loc>=size){  // deleting from end 
+
+			Node prev =head;
+			Node tmp = head;
+			while(tmp!=null){  // looping to get ythe 2nd last element 
+				prev=tmp;
+				tmp=tmp.next;			
+			}
+
+			prev.next=null;
+			tail=prev;
+
+		}
+		else {			// deleting from middle in the middle 
+			int pos =0;
+			Node prev =head;
+			Node tmp = head;
+			while(tmp!=null){
+				pos++;
+				if(pos==loc){  // changing the value 
+					prev.next=tmp.next;
+					break;
+				}else{
+					prev=tmp;
+					tmp=tmp.next;
+				}
+			}
+
+
+		}
+		size--;
+	}
 }
